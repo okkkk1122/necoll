801e48b69412af15c70cd77885a1fa69430c57e5
okkkk1122 <mghproud@gmail.com>
2026-06-27 13:38:50 +0330

    saturday
    config

--- store/src/app/page.tsx
+++ store/src/app/page.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from 'next';
+
 import HomePageLayout from '@/components/HomePageLayout';
+
+export const metadata: Metadata = {
+  other: {
+    enamad: '60974624',
+  },
+};
 
 export default function HomePage() {
   return <HomePageLayout />;
